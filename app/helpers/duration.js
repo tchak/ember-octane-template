252bfbc6134b4b00ef0abd31332113d89ec8db5b
Paul Chavard <git@paul.chavard.net>
2020-05-02 11:09:17 +0200

    interval -> duration

--- app/helpers/duration.js
+++ app/helpers/duration.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 import interval from 'human-interval';
 
-export default helper(function ([i]) {
+export default helper(function duration([i]) {
   return interval(i);
 });
